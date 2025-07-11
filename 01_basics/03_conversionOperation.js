5f6f8849e37e46fec22f988c493978c91e85cae3
Satyam kumar <satyamkumar07052004@gmail.com>
2025-07-11 15:26:19 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "Satyam"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNUmber =Number(score)
+// console.log(typeof valueInNUmber);
+// console.log(valueInNUmber);
+
+
+let isLoggedIn = "Satyam"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
